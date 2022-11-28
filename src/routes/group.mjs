c4fa2d2f6bb8617650ed8c28ce85d578876fca34
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2022-11-28 21:13:28 +0700

    Add route utilities

--- src/routes/group.mjs
+++ src/routes/group.mjs
@@ -1,64 +1,40 @@
 import express from "express";
 import * as db from "../database/quizDatabase.mjs";
+import { getGroup, getUsername, run } from "./routeUtils.mjs"
 
 const router = express.Router();
 
 router.get("/create", async (req, res) => {
 	let query = req.query;
-	await run(res, db.addGroup(group(query) ?? query.name, user(query) ?? query.creator));
+	await run(res, db.addGroup(getGroup(query) ?? query.name, getUsername(query) ?? query.creator));
 })
 
 router.get("/addUser", async (req, res) => {
 	let query = req.query;
-	await run(res, db.addGroupMember(group(query), user(query)));
+	await run(res, db.addGroupMember(getGroup(query), getUsername(query)));
 })
 
 router.get("/kickUser", async (req, res) => {
 	let query = req.query;
-	await run(res, db.removeGroupMember(group(query), user(query)));
+	await run(res, db.removeGroupMember(getGroup(query), getUsername(query)));
 })
 
 router.get("/updateUser", async (req, res) => {
 	let query = req.query;
-	await run(res, db.updateGroup(group(query), user(query), query.isOwner));
+	await run(res, db.updateGroup(getGroup(query), getUsername(query), query.isOwner));
 })
 
 router.get("/createdBy", async (req, res) => {
-	await run(res, db.getGroup(user(req.query), "creator"));
+	await run(res, db.getGroup(getUsername(req.query), "creator"));
 })
 
 router.get("/joinedBy", async (req, res) => {
 	let query = req.query;
-	await run(res, db.getGroup(group(query), user(query), query.isOwner));
+	await run(res, db.getGroup(getGroup(query), getUsername(query), query.isOwner));
 })
 
 router.get("/inviteUser", async (req, res) => {
 	res.send("OK");
 })
-
-function group(query) {
-	return query.group ?? query.groupname ?? query.groupName;
-}
-
-function user(query) {
-	return query.user ?? query.username ?? query.userName;
-}
-
-async function run(res, promise) {
-	try {
-		let data = await promise;
-		res.status(200).json({ 
-			success: true,
-			data: data
-		});
-	}
-	catch (err) {
-		console.log(err);
-		res.status(400).json({ 
-			success: false,
-			error: err.toString()
-		});
-	}
-}
 
 export default router;
---
+++ src/routes/routeUtils.mjs
@@ -0,0 +1,32 @@
+export function getGroup(query) {
+	return query.group ?? query.groupname ?? query.groupName;
+}
+
+export function getUsername(query) {
+	return query.user ?? query.username ?? query.userName;
+}
+
+export async function run(res, promise) {
+	try {
+		let data = await promise;
+        sendData(res, data);
+	}
+	catch (err) {
+        sendError(res, err);
+	}
+}
+
+export function sendData(res, data) {
+    res.status(200).json({ 
+        success: true,
+        data: data
+    });
+}
+
+export function error(res, err) {
+    console.log(err);
+    res.status(400).json({ 
+        success: false,
+        error: err.toString()
+    });
+}
