0a213dfc94195c422087a29ebc22cc5a87af8f1d
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2022-12-25 19:46:07 +0700

    Update presentation invite

--- src/auth/mail.mjs
+++ src/auth/mail.mjs
@@ -4,28 +4,20 @@ export default async function validateEmail(req, res, next){
     let query = req.query;
 
 	let receiver = query.receiver;
-	let presentId = getPresentationId(query) ?? query.id;
 
-	if (!receiver && !presentId) {
+	if (!receiver) {
 		sendError(res, "Missing data!");
         return;
 	}
 
 	let user = await db.getUser(receiver);
-    let presentation = await db.getPresentation(id);
 
 	if (!user) {
 		sendError(res, "User doesn't exist!");
         return;
 	}
 
-    if(!presentation){
-        sendError(res, "Present doesn't exist!");
-        return;
-    }
-
     req.receiver = user;
-    req.presentation = presentation;
 
     next();
 }
